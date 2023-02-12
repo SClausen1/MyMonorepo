79c9956d09900ebd04e5647e9eb22980e534233a
sclausen1 <stevenclausen1@gmail.com>
2023-02-12 00:51:18 -0500

    update deps,

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,12 @@
 import { Button } from "ui";
-
+import { Universe } from "sclausen1-wasm-game-of-life"
 export default function Web() {
+  const universe = Universe.new()
   return (
     <div>
       <h1>Web</h1>
       <Button />
+      
     </div>
   );
 }
